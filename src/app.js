48f98c7e29dd323bbb72bfe2f63aad42b5c07aa6
fmoralesaliaga <fmoralesaliaga@gmail.com>
2023-11-27 08:18:33 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const app = express();
 app.use(express.json()); // Middleware para parsear JSON en el cuerpo de las solicitudes
 
 // Utilizar las rutas definidas en 'routes/index.js'
+// '/api' es el prefijo de ruta para todas las rutas definidas en userRoutes
 app.use('/api', userRoutes);
 
 // Definir el puerto y poner el servidor a escuchar
