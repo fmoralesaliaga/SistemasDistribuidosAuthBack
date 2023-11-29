41bd11094423b97d20ac55bfb2e86675655e4f21
fmoralesaliaga <fmoralesaliaga@gmail.com>
2023-11-29 09:14:51 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const userRoutes = require('./routes');
-
 const app = express();
+const cors = require('cors');
+
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 app.use(express.json()); // Middleware para parsear JSON en el cuerpo de las solicitudes
 
 // Utilizar las rutas definidas en 'routes/index.js'
--- src/db/index.js
+++ src/db/index.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 
 // Conectarse a la base de datos SQLite. Si no existe, se crea una nueva en memoria.
 // Pueden utilizar un string con una ruta para crear un archivo o el string ':memory:' para crear una base de datos temporal en memoria.
-const db = new sqlite3.Database('./db/SDAuth.sqlite', (err) => {
+const db = new sqlite3.Database('./SDAuth.sqlite', (err) => {
   if (err) {
     console.error(err.message);
     throw err;
